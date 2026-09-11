65972f4f9b41e28b94d3e3b4444c909e2ab64016
forammarviya2805-star <forammarviya2805@gmail.com>
2026-09-11 10:24:50 +0530

    first commit

---
+++ Controllers/HomeController.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+exports.home = (req,res) =>{
+   res.render('home')
+}
--- html_rander_using _ejs/Controllers/HomeController.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-
-exports.home = (req,res) =>{
-   res.render('home')
-}
